443c89b3bfea299083eb0741d9efefce67561581
uhunkler <urs.hunkler@unodo.de>
2014-01-19 16:33:00 +0100

    Extend the plugin to get the template data from a defined object saved on the file object.
    I changed the data parameter to except either an object or a string. When a string is given the plugin creates a data object and sets the object to the property with the same name attached to the file object. The data can be set bey another plugin.
    
    I want to use a gulp workflow to process markdown files with a frontmatter for the page content and place the content into a saved html wrapper with header and footer. The frontmatter data is extracted from the markdown file and saved in a property (with a default or given name) on the file object by the frontmatter plugin. The wrap plugin can now read the frontmatter data when I set the data as a string with the property name.
    
    Signed-off-by: uhunkler <urs.hunkler@unodo.de>

--- index.js
+++ index.js
@@ -8,29 +8,47 @@ var PluginError = gutil.PluginError;
 
 var PLUGIN_NAME = 'gulp-wrap';
 
-function compile(contents, template, data, options){
+function compile(contents, template, data, options) {
   data = data !== undefined ? data : {};
   data.contents = contents;
   return tpl(template, data, options);
 }
 
-module.exports = function(opts, data, options){
+module.exports = function (opts, data, options) {
   if (!opts) {
     throw new PluginError(PLUGIN_NAME, PLUGIN_NAME + ': Missing template parameter');
   }
 
   var template;
 
-  if (typeof(opts) === 'object') {
+  if (typeof opts === 'object') {
     template = fs.readFileSync(opts.src, 'utf-8');
   } else {
     template = opts;
   }
 
-  function wrap(file, callback){
+  /**
+   * Wrap the stream content with the given template and data
+   *
+   * @param {object} file The file
+   * @param {function} callback The final callback to continue on the pipe
+   */
+  function wrap(file, callback) {
+    // If data is a string the given name is treated as a property
+    // on the file object. The data object is created and the 
+    // file property is copied to the data object.
+    if (typeof data === "string") {
+      var _prop = data;
+      data = {};
+      data[_prop] = file[_prop];
+    }
+
     if (gutil.isStream(file.contents)) {
       var through = es.through();
-      var wait = es.wait(function(err, contents){
+      var wait = es.wait(function (err, contents) {
+        if (err) {
+          throw new PluginError(PLUGIN_NAME, PLUGIN_NAME + ': a wait error occured');
+        }
         through.write(compile(contents, template, data, options));
         through.end();
       });
