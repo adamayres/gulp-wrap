e0f426b2101739d5fb329da16691ffb5bca67bea
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-01-16 05:53:38 +0900

    Read template file asynchronously

--- index.js
+++ index.js
@@ -5,28 +5,29 @@ var path = require('path');
 var BufferStreams = require('bufferstreams');
 var consolidate = require('consolidate');
 var extend = require('node.extend');
-var fs = require('graceful-fs');
 var PluginError = require('gulp-util').PluginError;
+var ES6Promise = global.Promise || require('es6-promise').Promise;
+var readFile = require('fs-readfile-promise');
 var through = require('through2');
+var tryit = require('tryit');
 
 var PLUGIN_NAME = 'gulp-wrap';
 
-module.exports = function(opts, data, options){
-  if (!opts) {
-    throw new PluginError(PLUGIN_NAME, PLUGIN_NAME + ': Missing template parameter');
-  }
-
-  var template;
-
+module.exports = function gulpWrap(opts, data, options) {
+  var promise;
   if (typeof opts === 'object') {
     if (typeof opts.src !== 'string') {
-      throw new PluginError(PLUGIN_NAME, 'Expecting `src` option.');
+      throw new PluginError(PLUGIN_NAME, new TypeError('Expecting `src` option.'));
     }
-    template = fs.readFileSync(opts.src, 'utf8');
+    promise = readFile(opts.src, 'utf8');
   } else {
-    template = opts;
+    if (typeof opts !== 'string' && typeof opts !== 'function') {
+      throw new PluginError(PLUGIN_NAME, 'Template must be a string or a function.');
+    }
+
+    promise = ES6Promise.resolve(opts);
   }
-  
+
   data = data || {};
   options = options || {};
 
@@ -34,7 +35,7 @@ module.exports = function(opts, data, options){
     options.engine = 'lodash';
   }
 
-  return through.obj(function(file, enc, cb){
+  function gulpWrapTransform(file, enc, cb) {
     var self = this;
 
     if (file.isNull()) {
@@ -42,73 +43,72 @@ module.exports = function(opts, data, options){
       return;
     }
 
-    function compile(contents, done){
+    function compile(contents, done) {
       // attempt to parse the file contents for JSON or YAML files
       if (options.parse !== false) {
         var ext = path.extname(file.path).toLowerCase();
-        
-        try {
+
+        tryit(function() {
           if (ext === '.json') {
             contents = JSON.parse(contents);
-          } else if (ext === '.yml' || ext === '.yaml' ) {
+          } else if (ext === '.yml' || ext === '.yaml') {
             contents = require('js-yaml').safeLoad(contents);
           }
-        } catch (err) {
+        }, function(err) {
+          if (!err) {
+            return;
+          }
           throw new PluginError(PLUGIN_NAME, PLUGIN_NAME + ': error parsing ' + file.path);
-        }
+        });
       }
 
-      /*
-       * Add `file` field to source obj used when interpolating
-       * the template. Ensure the user supplied data object is not
-       * augmented to prevent file prop leakage across multiple
-       * streams. Properties specified in the user supplied data
-       * object should take precedence over properties supplied
-       * by the file.
-       */
-      var newData = extend({ file: file }, options, data, file.data, { contents: contents });
+      var newData = extend({file: file}, options, data, file.data, {contents: contents});
 
-      /*
-       * Allow template to be a function, pass it the data object.
-       */
-      if (typeof template === 'function') {
-        template = template(newData);
-      }
-
-      consolidate[options.engine].render(template, newData, function(err, result) {
-        if (err) {
-          done(new PluginError(PLUGIN_NAME, err));
-          return;
+      promise.then(function(template) {
+        if (typeof template === 'function') {
+          template = template(newData);
         }
 
-        done(null, new Buffer(result));
-      });
+        consolidate[options.engine].render(template, newData, function(err, result) {
+          if (err) {
+            done(new PluginError(PLUGIN_NAME, err));
+            return;
+          }
+          done(null, new Buffer(result));
+        });
+      }, done);
     }
 
     if (file.isStream()) {
       file.contents = file.contents.pipe(new BufferStreams(function(none, buf, done) {
-        compile(buf, function(err, contents){
-          if (err) {
-            self.emit('error', err);
-            done(err);
-          } else {
-            done(null, contents);
-            self.push(file);
-          }
-          cb();
+        compile(buf, function(err, contents) {
+          process.nextTick(function() {
+            if (err) {
+              self.emit('error', err);
+              done(err);
+            } else {
+              done(null, contents);
+              self.push(file);
+            }
+            cb();
+          });
         });
       }));
       return;
-    };
+    }
 
     compile(file.contents, function(err, contents) {
-      if (err) {
-        self.emit('error', err);
-      } else {
-        file.contents = contents;
-        self.push(file);
-      }
-      cb();
+      process.nextTick(function() {
+        if (err) {
+          self.emit('error', err);
+        } else {
+          file.contents = contents;
+          self.push(file);
+        }
+        cb();
+      });
     });
-  });
+  }
+
+  return through.obj(gulpWrapTransform);
 };
